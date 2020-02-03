f753ed027a6f64cd726fcabe0e360d0952648bb6
Alex Gorodetsky <gorod1974@hotmail.com>
2020-02-04 01:56:43 +0300

    test-001

---
+++ js/main.js
@@ -0,0 +1,41 @@
+
+setTimeout(function() {
+    document.getElementById('object_1').style.display = 'block';
+    document.getElementById('object_2').style.display = 'block';
+    document.getElementById('object_3').style.display = 'block';
+    document.getElementById('object_4').style.display = 'block';
+    document.getElementById('object_5').style.display = 'block';
+    document.getElementById('object_6').style.display = 'block';
+    document.getElementById('object_7').style.display = 'block';
+    document.getElementById('object_8').style.display = 'block';
+}, 1000);
+// loading disable
+setTimeout(function() {
+    document.getElementById('loading').style.display = 'none';
+}, 4500);
+
+// hover in button
+$(document).ready(function () {
+    $('button.animated' || 'a.animated').hover(
+        function () {
+            $(this).addClass('pulse'); // Добавляем класс bounce
+        },
+        function () {
+            $(this).removeClass('pulse'); // Убираем класс
+        }
+    )
+});
+
+
+
+$('.test-popup-link').magnificPopup({
+    type: 'image'
+    // other options
+  });
+
+  $('.parent-container').magnificPopup({
+    delegate: 'a', // child items selector, by clicking on it popup will open
+    type: 'image',
+    gallery:{enabled:true}
+    // other options
+  });
